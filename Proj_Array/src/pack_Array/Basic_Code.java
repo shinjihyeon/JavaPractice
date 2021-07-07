473158970f32ce1f80f4e30bcaa522d202a70627
shinjihyeon <gi305@naver.com>
2021-07-07 19:47:50 +0900

    Java 배열 기초

---
+++ Proj_Array/src/pack_Array/Basic_Code.java
@@ -0,0 +1,21 @@
+package pack_Array;
+
+public class Basic_Code {
+
+	public static void main(String[] args) {
+
+		// 일반변수
+		int a = 5;
+		a++;
+		System.out.println("변수 a의 값 : " + a);
+		
+		// 배열변수
+		// 배열변수는 변수를 만들고 배열을 지정한다.
+		int[] b;
+		// 배열 개수를 선정한다.
+		b = new int[3];
+		// 그 후 엔덱스(index)번호를 0번부터 사용한다.
+		// 그 외 사용법은 일반변수와 동일함
+	}
+
+}
